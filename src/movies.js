f64f96745fecaad7304b34f448373d0ce2d099f6
Marine Le Cleach <lecleach.m@gmail.com>
2023-11-27 14:37:18 +0100

    iteration 1,2,3

--- src/movies.js
+++ src/movies.js
@@ -1,13 +1,59 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(moviesArray) {}
+function getAllDirectors(moviesArray) {
+    const dirArray = moviesArray.map((currentMovie) => {
+        return currentMovie.director;
+    });
+    return dirArray;
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {}
+function howManyMovies(moviesArray) {
+    
+   //currentMovie.genre.idexOf('Drama')
+     const stevenMovie =  moviesArray.filter((currentMovie) => (currentMovie.director == 'Steven Spielberg' ) );
+    let dramaMovie = [];
+        stevenMovie.forEach((currentMovie) => {
+        if(currentMovie.genre.includes('Drama')){
+            dramaMovie.push(currentMovie);
+        }
+     });
+        
+    if(stevenMovie.length === 0){
+        return 0;
+    }else {
+        return dramaMovie.length;
+    }
+}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
+function scoresAverage(moviesArray) {
+
+    const movieWithScore =  moviesArray.filter((currentMovie) => (currentMovie.score != '' && currentMovie.score != undefined) );
+
+   
+  let score =0;
+  score =  movieWithScore.reduce((accumulator, movieWithScore) =>{
+        
+            return accumulator + movieWithScore.score;
+        
+    
+    }, 0);
+
+    if(score != 0 ){
+    score = (score / moviesArray.length);
+ 
+    }
+    if(moviesArray.length != 0){
+    return score;
+    }else{
+        return 0;
+    }
+
+   
+
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 function dramaMoviesScore(moviesArray) {}
